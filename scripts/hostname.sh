7c2bd2185eb53d29816d163ff559f4d6cae8c7ba
Jangshant Singh <jangshant@gmail.com>
2016-08-25 14:27:01 +0530

    Update hostname.sh

--- scripts/hostname.sh
+++ scripts/hostname.sh
@@ -5,8 +5,8 @@ read input_variable1
 echo "Please enter domain name: "
 read input_variable2
 echo "You entered: $input_variable1.$input_variable2"
-sed -i "1s/.*/$input_variable1/"  /etc/hostname
+sudo sed -i "1s/.*/$input_variable1/"  /etc/hostname
 ip_address=$(/sbin/ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
 ipa="$ip_address"
-sed -i "s/$ipa/#$ipa/g" /etc/hosts
-echo "$ipa $input_variable1.$input_variable2">> /etc/hosts
+sudo sed -i "s/$ipa/#$ipa/g" /etc/hosts
+sudo echo "$ipa $input_variable1.$input_variable2">> /etc/hosts
